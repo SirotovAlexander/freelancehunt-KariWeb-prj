ae0686437b93b41f2c089adf852442b59723cd21
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-21 23:06:07 +0200

    update calc section
    update calc section

---
+++ src/js/round-slider.js
@@ -0,0 +1,10 @@
+$('#slider').roundSlider({
+  handleShape: 'dot',
+  radius: 170,
+  value: 0,
+  handleSize: '+25',
+  startAngle: 90,
+  lineCap: 'square',
+  sliderType: 'min-range',
+  width: 12,
+});
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import './js/modal-open';
+import './js/round-slider';
