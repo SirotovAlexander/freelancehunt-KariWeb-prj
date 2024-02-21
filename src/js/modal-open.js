90c820afacb4e98ffd057aff3e927e59048c8a0a
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-21 22:28:18 +0200

    add input
    add input

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -1,9 +1,12 @@
 const buttonOpen = document.querySelector('.button__wrapper');
+const buttonOpenTwo = document.querySelector('.button__wrapper.secondButton');
 const modalWindow = document.querySelector('[data-modal="modal-wholesale"]');
 const closeButton = document.querySelector('.mClose-fix');
-console.dir(closeButton);
+console.dir(buttonOpenTwo);
 
 buttonOpen.addEventListener('click', onClick);
+buttonOpenTwo.addEventListener('click', onClick);
+
 closeButton.addEventListener('click', onClose);
 function onClick(event) {
   modalWindow.classList.remove('is-hidden');
