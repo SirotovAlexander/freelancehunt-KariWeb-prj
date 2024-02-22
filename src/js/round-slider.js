b1cdcac5009c8cd9400e36f3d57dcc5fc9f71e38
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-23 00:13:57 +0200

    Update round-slider.js

--- src/js/round-slider.js
+++ src/js/round-slider.js
@@ -7,6 +7,13 @@ $('#slider').roundSlider({
   lineCap: 'square',
   sliderType: 'min-range',
   width: 12,
+  change: function (args) {
+    console.log(args.value);
+    $('#range').html(args.value);
+  },
+  // drag: function (args) {
+  //   $('#range1').html(args.value);
+  // },
 });
 
-console.log($('#slider').roundSlider()[0].innerText);
+console.dir($('#slider').roundSlider()[0]);
