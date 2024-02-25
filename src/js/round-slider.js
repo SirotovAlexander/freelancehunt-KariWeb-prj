0700cdc44f92cd26bc8069eeebe924add2708fbd
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-25 03:17:54 +0200

    half done slider calc

--- src/js/round-slider.js
+++ src/js/round-slider.js
@@ -1,5 +1,22 @@
+const valueQuantity = document.querySelector('.product__valueFirst');
+const valueMinimal = document.querySelector('.product__valueSecond');
+const rangeValueCalculator = document.querySelector(
+  '.styled-slider.slider-progress'
+);
+const sliderValue = document.querySelector('.sliderValue');
+// const profitValue = document.querySelector(
+//   '."profit-wrapper__text.profit-wrapper__text--fix'
+// );
+// console.dir(valueQuantity.textContent);
+// console.dir(sliderValue.textContent);
+// console.dir(rangeValueCalculator);
+rangeValueCalculator.addEventListener('input', textChange);
+
+function textChange(evt) {
+  valueMinimal.textContent = `${evt.currentTarget.valueAsNumber}`;
+}
+
 const screenWidth = window.screen.width;
-console.log(screenWidth);
 
 let sliderWidth = 0;
 let sliderID = '';
@@ -24,12 +41,13 @@ $(`${sliderID}`).roundSlider({
   sliderType: 'min-range',
   width: 12,
   change: function (args) {
-    console.log(args.value);
+    // console.log(args.value);
     $('#range').html(args.value);
+    valueQuantity.textContent = args.value;
+    sliderValue.textContent = '';
+    sliderValue.textContent = args.value + '' + '.шт';
   },
   // drag: function (args) {
   //   $('#range1').html(args.value);
   // },
 });
-
-// console.dir($('#slider').roundSlider()[0]);
