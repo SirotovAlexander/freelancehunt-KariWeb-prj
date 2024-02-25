7725ea5b3938f1d52ab3ac6842bb3422d41efc61
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-25 14:22:09 +0200

    Update modal-open.js

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -1,15 +1,13 @@
-const buttonOpen = document.querySelector('.button__wrapper');
-const buttonOpenTwo = document.querySelector('.button__wrapper.secondButton');
+const buttonsOpen = document.querySelectorAll('.hero__button');
 const modalWindow = document.querySelector('[data-modal="modal-wholesale"]');
 const closeButton = document.querySelector('.mClose-fix');
-
-buttonOpen.addEventListener('click', onClick);
-buttonOpenTwo.addEventListener('click', onClick);
-
+// console.log(buttonsOpen);
+buttonsOpen.forEach(item => {
+  item.addEventListener('click', e => {
+    modalWindow.classList.toggle('is-hidden');
+  });
+});
 closeButton.addEventListener('click', onClose);
-function onClick(event) {
-  modalWindow.classList.remove('is-hidden');
-}
 function onClose(event) {
   modalWindow.classList.add('is-hidden');
 }
