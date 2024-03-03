008f6eac3b8c7e2b20218a30f78049d22c152b35
SirotovAlexander <alexander.sirotov@gmail.com>
2024-03-03 13:17:47 +0200

    update
    update

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -1,12 +1,18 @@
 const buttonsOpen = document.querySelectorAll('.hero__button');
-const modalWindow = document.querySelector('[data-modal="modal-wholesale"]');
+const modalWindow = document.getElementById('contacModalWindowFix');
 const closeButton = document.querySelector('.mClose-fix');
 
+// buttonsOpen.forEach(item => {
+//   item.addEventListener('click', e => {
+//     modalWindow.classList.toggle('is-hidden');
+//   });
+// });
 buttonsOpen.forEach(item => {
   item.addEventListener('click', e => {
-    modalWindow.classList.toggle('is-hidden');
+    modalWindow.classList.remove('is-hidden');
   });
 });
+
 closeButton.addEventListener('click', onClose);
 function onClose(event) {
   modalWindow.classList.add('is-hidden');
