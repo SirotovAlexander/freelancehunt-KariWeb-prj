e991691ebd337e7a4e961c425ee423304a43b92a
SirotovAlexander <alexander.sirotov@gmail.com>
2024-03-11 01:58:21 +0200

    Update langDropdownOpen.js

--- src/js/langDropdownOpen.js
+++ src/js/langDropdownOpen.js
@@ -1,9 +1,9 @@
-// const choseButton = document.querySelector('.chose_lang');
-// const dropLangMenue = document.querySelector('.lang-block');
+const choseButton = document.querySelector('.chose_lang');
+const dropLangMenue = document.querySelector('.lang-block');
 
-// choseButton.addEventListener('click', langDropOpen);
+choseButton.addEventListener('click', langDropOpen);
 
-// function langDropOpen(e) {
-//   choseButton.classList.toggle('active');
-//   dropLangMenue.classList.toggle('show');
-// }
+function langDropOpen(e) {
+  choseButton.classList.toggle('active');
+  dropLangMenue.classList.toggle('show');
+}
