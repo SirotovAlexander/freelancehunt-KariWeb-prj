8d349354610d0fb54adc88d6a7bc30df0f55c0c5
SirotovAlexander <116684295+SirotovAlexander@users.noreply.github.com>
2024-03-10 18:47:15 +0000

    Deploying to gh-pages from @ SirotovAlexander/freelancehunt-KariWeb-prj@647ea947a21db841802d6af747c4a8d6e406e61e 🚀

--- js/langDropdownOpen.js
+++
@@ -1,9 +0,0 @@
-const choseButton = document.querySelector('.chose_lang');
-const dropLangMenue = document.querySelector('.lang-block');
-
-choseButton.addEventListener('click', langDropOpen);
-
-function langDropOpen(e) {
-  choseButton.classList.toggle('active');
-  dropLangMenue.classList.toggle('show');
-}
--- main.js
+++
@@ -1,8 +0,0 @@
-import './js/modal-open';
-import './js/round-slider';
-import './js/range-input';
-import './js/goodsGalery';
-import './js/sliderGalery';
-import './js/video-modal';
-import './js/goodsGaleryModal';
-import './js/langDropdownOpen';
