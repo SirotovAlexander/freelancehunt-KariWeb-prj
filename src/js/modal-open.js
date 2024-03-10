003009b28f5af913d23bcedcf41e34affa8b5f34
SirotovAlexander <alexander.sirotov@gmail.com>
2024-03-11 00:54:50 +0200

    asd
    asd

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -6,22 +6,10 @@ const closeButton = document.querySelector('.mClose-fix');
 buttonsOpen.forEach(item => {
   item.addEventListener('click', e => {
     modalWindow.classList.remove('is-hidden');
-    const value = `-${window.scrollY}px`;
-
-    bodyElem.style.position = 'fixed';
-    bodyElem.style.top = value;
   });
 });
 
 closeButton.addEventListener('click', onClose);
 function onClose(event) {
   modalWindow.classList.add('is-hidden');
-  // const value = `-${window.scrollY}px`;
-  // console.log(window.scrollY);
-  const value = window.scrollY;
-  // window.scrollTo(0, 684);
-  bodyElem.style.position = '';
-  bodyElem.style.top = '';
-  window.scrollTo(0, 684);
 }
-// window.scrollTo(0, 684);
